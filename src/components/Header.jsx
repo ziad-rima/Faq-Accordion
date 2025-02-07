668e2d9b898e865dbb531428f5c6749abd99028e
Ziad Rima <ziadrima49@gmail.com>
2025-02-07 10:41:14 +0100

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import StarIcon from "./StarIcon"
+const Header = () => {
+  return (
+    <div className="header-component">
+      <StarIcon />
+      <h1 className="header-title">FAQs</h1>
+    </div>
+  )
+}
+
+export default Header
