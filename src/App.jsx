7b1856caa67b84f9df0e6e88e4e915712d0e0c0e
Ziad Rima <ziadrima49@gmail.com>
2025-02-08 10:38:15 +0100

    Style and render faqs section components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./components/Header"
+import Faqs from "./components/Faqs"
 const App = () => {
   return (
     <div className="main-component">
       <Header />
+      <Faqs />
     </div>
   )
 }
