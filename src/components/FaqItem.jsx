38926b14d93db0588095bac8630ba5b6037211af
Ziad Rima <ziadrima49@gmail.com>
2025-02-08 10:37:19 +0100

    create faq section components

---
+++ src/components/FaqItem.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import PlusIcon from "./PlusIcon"
+import MinusIcon from "./MinusIcon"
+const FaqItem = ({question, answer}) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+    <div className="faq-item">
+        <div className="question-container">
+            <div className="question">{question}</div>
+            <button onClick={() => setIsOpen(!isOpen)} aria-expanded={isOpen ? "Collapse answer" : "Expand answer"}>
+                {!isOpen ? <PlusIcon /> : <MinusIcon />}
+            </button>
+        </div>
+        
+        <div className={`answer-container ${isOpen ? 'open' : ''}`}>
+            <p className="answer">{answer}</p>
+        </div>
+    </div>
+  )
+}
+
+export default FaqItem
